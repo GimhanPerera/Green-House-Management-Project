afba3d4d560eb292cf29d7eebf5b6ec24c1a9f23
Bimindu-aberathna <aberath-im20004@stu.kln.ac.lk>
2024-05-16 15:25:22 +0530

    home page includine drawer and header

--- Frontend/src/pages/loginPage.jsx
+++ Frontend/src/pages/loginPage.jsx
@@ -3,11 +3,12 @@ import { Box } from '@mui/material';
 export const LoginPage = () => {
 
     return (
-        <>
+        <div style={{backgroundColor:'red'}}>
             <Box component="div" sx={{}}>
-                Login page here
+                <h1 >Login page here</h1>
+                hello world
             </Box>
-        </>
+        </div>
     )
 
 }
