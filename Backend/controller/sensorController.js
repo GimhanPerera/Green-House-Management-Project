5f96e453a6a9fb694bb7b605f7c15c0aa23545d6
Gimhan1 <rashmithagimhan32@gmail.com>
2024-06-16 02:52:24 +0530

    Email sending bug fixed

--- Backend/controller/sensorController.js
+++ Backend/controller/sensorController.js
@@ -1,6 +1,6 @@
-const { history, sensor,user } = require('../models');
-const {validateToken} = require('../JWT');
-const {sendAlert} = require('./alertController');
+const { history, sensor, user } = require('../models');
+const { validateToken } = require('../JWT');
+const { sendAlert } = require('./alertController');
 
 const getSensorHistoryById = async (req, res) => {
     try {
@@ -9,7 +9,7 @@ const getSensorHistoryById = async (req, res) => {
                 sensorSensorId: req.params.id,
             },
         });
-        
+
         res.status(200).json(historyList);
     } catch (error) {
         console.error(error)
@@ -21,10 +21,10 @@ const getAllSensorDataOfUser = async (req, res) => {
     try {
         const sensorList = await sensor.findAll({
             where: {
-                	userUserId : req.userId
+                userUserId: req.userId
             },
         });
-        console.log("ID ",req.userId)
+        console.log("ID ", req.userId)
         res.status(200).json(sensorList);
     } catch (error) {
         console.error(error)
@@ -65,28 +65,28 @@ const receiveSensorData = async (req, res) => {
             return res.status(404).json({ message: 'Sensor not found' });
         }
         const userId = parseInt(sensor1.userUserId, 10);
-        console.log("userID ",userId)
+        console.log("userID ", userId)
         //const user1 = await user.findOne({ where: { userId } });
         //const email = user1.email;
-        if (measurement > sensor1.upper_limit ) {
+        const sensorDetails = {
+            sensorId: sensor1.sensorId,
+            sensorName: sensor1.sensorName,
+            upper_limit: sensor1.upper_limit,
+            lower_limit: sensor1.lower_limit,
+            value: measurement
+        }
+        if (measurement > sensor1.upper_limit) {
             const newHistory = await history.create({
                 dateTime: new Date(),
                 measurement,
                 status: 'High',
                 sensorSensorId: sensorId
             });
-            const sensorDetails = {
-                sensorId: sensor1.sensorId,
-                sensorName: sensor1.sensorName,
-                upper_limit: sensor1.upper_limit,
-                lower_limit: sensor1.lower_limit,
-                value: measurement
-            }
             console.log("Upper")
-            const answer = sendAlert(userId,sensorDetails)
+            const answer = sendAlert(userId, sensorDetails)
             return res.status(200).json(sensorDetails);
         }
-        
+
         if (measurement < sensor1.lower_limit) {
             const newHistory = await history.create({
                 dateTime: new Date(),
@@ -94,8 +94,8 @@ const receiveSensorData = async (req, res) => {
                 status: 'Low',
                 sensorSensorId: sensorId
             });
-          const answer = sendAlert(userId,sensorDetails)
-            return res.status(201).json(newHistory);
+            const answer = sendAlert(userId, sensorDetails)
+            return res.status(201).json(sensorDetails);
         }
 
         // Get the current max historyId
@@ -132,7 +132,7 @@ const receiveSensorData = async (req, res) => {
 
 const editSensor = async (req, res) => {
     try {
-        
+
         const editSensor = await sensor.findByPk(req.body.sensorId);
         // Check if the sensor exists
         if (!editSensor) {
@@ -141,8 +141,8 @@ const editSensor = async (req, res) => {
         }
         editSensor.sensorName = req.body.sensorName;
         editSensor.description = req.body.description;
-        editSensor.upper_limit=req.body.upper_limit;
-        editSensor.lower_limit=req.body.lower_limit;
+        editSensor.upper_limit = req.body.upper_limit;
+        editSensor.lower_limit = req.body.lower_limit;
         editSensor.unit = req.body.unit;
         await editSensor.save();
         res.status(201).json("Updated");
