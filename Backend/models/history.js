e97a8cbc0f730e4e7bfca7ab24bfd7f97add8f8c
mohdzafry <mohdzafry7@gmail.com>
2024-06-15 16:22:04 +0530

    history.js

--- Backend/models/history.js
+++ Backend/models/history.js
@@ -28,26 +28,36 @@
 // }
 
 // models/history.js
-module.exports = (sequelize, DataTypes) => {
-  const History = sequelize.define('History', {
-    historyId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true, // Set auto increment
-    },
-    dateTime: {
-      type: DataTypes.DATE,
-    },
-    measurement: {
-      type: DataTypes.FLOAT,
-    },
-    status: {
-      type: DataTypes.STRING,
-    }
-  }, {
-    timestamps: false // Disable timestamps
-  });
 
-  return History;
-};
+module.exports = (sequelize, DataTypes) => {
+    const History = sequelize.define('History', {
+      historyId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      dateTime: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+      },
+      measurement: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+      },
+      status: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      sensorSensorId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Sensor', // Ensure 'Sensor' is the correct model name
+          key: 'sensorId'
+        }
+      }
+    });
+  
+    return History;
+  };
+  
